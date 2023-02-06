df04007d725618996711ac72f0350c04e6d8a47c
Samwel Charles <younggeeks101@gmail.com>
2023-02-06 08:10:22 +0300

    Generic types, Traits, lifetimes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,64 @@
+use std::fmt::{Debug, Display};
+
 fn main() {
-    println!("Hello, world!");
+    let tweet = Tweet {
+        username: "samjunior".to_string(),
+        content: String::from("This is probably a lorem text, should've been an email"),
+        reply: false,
+        retweet: false,
+    };
+
+    notify(&tweet);
+}
+
+pub trait Summary {
+    fn summarize_author(&self) -> String;
+    fn summarize(&self) -> String {
+        format!("Read more from {}...", self.summarize_author())
+    }
+}
+
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summary for NewsArticle {
+    fn summarize_author(&self) -> String {
+        format!("")
+    }
+}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Debug for Tweet {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("Tweet")
+            .field("username", &self.username)
+            .field("content", &self.content)
+            .field("reply", &self.reply)
+            .field("retweet", &self.retweet)
+            .finish()
+    }
+}
+impl Display for Tweet {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{},{}", self.username, self.content)
+    }
+}
+impl Summary for Tweet {
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.username)
+    }
+}
+
+pub fn notify<T: Summary + Display + Debug>(item: &T) {
+    println!("Breaking news! {}", item.summarize())
 }
